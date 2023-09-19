f642c9932a5b79b3481ec8c92a1f78762c0c29ea
l.knippenberg <lukas.knippenberg@noz-digital.de>
2023-09-19 08:49:26 +0200

    run kmeans with react/next.js

---
+++ pp-2023/src/app/kmeans/kmeans.js
@@ -0,0 +1,5 @@
+import kMeans from 'node_modules/kmeansjs'
+
+export function useKMeans(dataset,b) {
+    return (dataset + b);
+}
--- pp-2023/src/app/kmeans/page.js
+++ pp-2023/src/app/kmeans/page.js
@@ -1,5 +1,31 @@
-export default function kmeans() {
-    return (
-        <h1>Hello World</h1>
-    );
-};
+import Script from 'next/script';
+import { useKMeans } from './kmeans';
+import bootstrap 
+
+export default function SiteKMeans () {
+
+    const custumDataSet = function () {
+        document.addEventListener('DOMContentLoaded', () => {
+
+        });
+    }
+    const basicSet = [
+        [1,5,7],
+        [-1,-5,8],
+        [15,8,9],
+        [12,7,8],
+        [0,2,6],
+        [8,4,9],
+        [10,2,4],
+        [9,1,-5],
+    ];
+
+
+    const hereWeGo = useKMeans(basicSet, "World");
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
