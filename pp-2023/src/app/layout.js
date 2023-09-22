cd2c57c7c63d57318042fe29b296231f636c8cfc
Darian Gerent <darian.gerent19@gmail.com>
2023-09-22 10:00:02 +0200

    refactor: imported google fonts

--- pp-2023/src/app/layout.js
+++ pp-2023/src/app/layout.js
@@ -1,8 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './globals.css'
-import { Inter } from 'next/font/google'
+/* import { Inter } from 'next/font/google' */
+import { Roboto, Playfair_Display } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+export const roboto = Roboto({ 
+  weight: ['400', '500'],
+  subsets: ['latin'],
+})
+
+export const playfair = Playfair_Display({
+  subsets: ['latin']
+})
 
 export const metadata = {
   title: 'PROG 2023 - KMeans',
@@ -12,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
