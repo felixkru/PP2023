555db64fe815617244a0f5cbcd5a382d02994ec8
felixkru <felix_kruse@hotmail.com>
2023-09-23 17:18:28 +0200

    test: test Datasets angelegt. +Kommentare

--- pp-2023/src/app/kmeans/scatter-chart.js
+++ pp-2023/src/app/kmeans/scatter-chart.js
@@ -7,24 +7,32 @@ function ScatterChart() {
         var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'scatter',
+            // hier sind die verschiedenen Datensets, alle Punkte eines Datensets welches mit "data:" beginnt bekommen die gleiche Farbe.
             data: {
                 datasets: [{
                     data: [
                         { x: 17, y: 3 },
-                    ]
-                   
+                        { x: 11, y: 3 } 
+                    ],
+                   label: "Set 1"
 
                 }, {
                     data: [
                         { x: 10, y: 3},
-                    ]
-                    
+                    ],
+                    label: "Set 2"
 
                 }, {
                     data: [
                         { x: 4, y: 14},
-                    ]
-                 
+                    ],
+                    label: "Set 3"
+                },
+                {
+                    data: [
+                        { x: 3, y: 11 }
+                    ],
+                    label: "Set 4"
                 }
                 ]
             },
@@ -32,7 +40,7 @@ function ScatterChart() {
                 scales: {
                     xAxes: [{
                         scaleLabel: {
-                            display: true,
+                            display: false,
                             labelString: 'x-Achse'
                         }
                     }],
@@ -49,7 +57,7 @@ function ScatterChart() {
     return (
         <>
             {/* Scatter chart */}
-            <h1 className="w-[150px] mx-auto mt-10 text-xl font-semibold capitalize ">Scatter Chart</h1>
+            <h1 className="mx-auto mt-10 text-xl font-semibold capitalize ">Scatter Chart</h1>
             <div className="w-[1100px] h-screen flex mx-auto my-auto">
                 <div className='border border-gray-400 pt-0 rounded-xl  w-full h-fit my-auto  shadow-xl'>
                     <canvas id='myChart'></canvas>
