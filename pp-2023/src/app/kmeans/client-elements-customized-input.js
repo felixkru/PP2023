4f518a669474f54a5378440ba6e0530a440bcccf
l.knippenberg <lukas.knippenberg@noz-digital.de>
2023-09-23 13:38:33 +0200

    feature - Erweiterung der Logik, dass die Eingaben in einem Array gespeichert und abgerufen werden können, zu Laufzeit.

--- pp-2023/src/app/kmeans/client-elements-customized-input.js
+++ pp-2023/src/app/kmeans/client-elements-customized-input.js
@@ -12,10 +12,8 @@ function CustomElementsCustomizedInput() {
     const [inputValues, setInputValues] = useState([]);
     const [disableButton, setDisableButton] = useState(false);
 
-    useEffect(() => {
-
-
-    }, []);
+    // Deklaration des Eingabe-Array
+    const dataArray = [];
 
     // Funktion legt fest, ob 2 oder 3 Button angezeigt werden.
     const handleButtonPoints = () => {
@@ -26,14 +24,19 @@ function CustomElementsCustomizedInput() {
         renderInputs();
     };
 
-    const handleInputChange = (index, value) => {
-
-
+    // Die Funktion wird bei jedem Input abgerufen und returnt ein Array mit den aktuellen Eingaben.
+    const handleInputChange = (col, row, value) => {
+        console.log(row)
+        if (!dataArray[row]){
+            dataArray.push([]);
+        }
+        dataArray[row][col] = value;
     };
 
+    // Funktion erhöht eine Statusvariable
     const handleButtonAdd = () => {
         setCounterCountAdd((click) =>
-        counterCountAdd + 1);
+            click + 1);
     };
 
     // Funktion erstellt eine HelperVariable die die Werte von 1-3 annimmt.
@@ -46,10 +49,28 @@ function CustomElementsCustomizedInput() {
         return helper;
     }
 
+    //Die Funktion nimmt einen entsprechenden rowIndex entgegen und passt diesen Wert dynamisch an
+    const setCorrectRowIndex = (rowIndex, colIndex) => {
+        if (colIndex >= counterCountFiles -1) {
+            rowIndex = rowIndex + 1;
+        }
+        return rowIndex;
+    }
+
+    // Die Funktion nimmt einen entsprechenden ColIndex und passt diesen Wert dynamisch an
+    const setCorrectColIndex = (colIndex) => {
+        if (colIndex < counterCountFiles - 1) {
+            colIndex = colIndex + 1;
+        } else {
+            colIndex = 0;
+        }
+        return colIndex;
+    }
+
     // Die Funktion rendert dynamische Inputs
     // i = aktueller unique Index und Key, helper = Hiflsvariable für die Beschriftung,
     // inputValue = Wertevariable des aktuellen Feldes, handleInputChange = Funktion
-    const renderInputElement = (i, helper, inputValue, handleInputChange) => (
+    const renderInputElement = (i, helper, inputValue, handleInputChange, colIndex, rowIndex) => (
         <div key={i} className={gridCell}>
             <label className='input-group-text' htmlFor={`inputVector${i}`}>{helper} Variable ...</label>
             <input id={`inputVector${i}`}
@@ -57,7 +78,9 @@ function CustomElementsCustomizedInput() {
                    type={"number"}
                    required={true}
                    value={inputValues[i]}
-                   onChange={ (event) => handleInputChange(i, event.target.value)}
+                    aria-rowindex={rowIndex}
+                   aria-colindex={colIndex}
+                   onChange={(event) => handleInputChange(colIndex, rowIndex, event.target.value)}
             />
         </div>
     );
@@ -65,22 +88,26 @@ function CustomElementsCustomizedInput() {
     // Funktion generiert Input Elemente durch Aufruf von renderInputElement mit den korrekten Parametern
     // inputValuesMatrix wird zurückgegeben mit den entsprechenden Werten
     const renderInputs = () => {
-        const inputValuesMatrix = [];
         if (counterCountFiles) {
             const newCounter = counterCountFiles * counterCountAdd;
             const inputs = [];
             let helper = 1;
+            let rowIndex = 0;
+            let colIndex = 0;
 
             for (let i = 0; i < newCounter; i++) {
                 helper = setCorrectHelperValue(helper);
                 inputs.push(
-                    renderInputElement(i, helper, inputValues[i], handleInputChange)
+                    renderInputElement(i, helper, inputValues[i],
+                        handleInputChange, colIndex, rowIndex)
                 );
+
+                rowIndex = setCorrectRowIndex(rowIndex, colIndex);
+                colIndex = setCorrectColIndex(colIndex);
                 helper = helper + 1;
             }
             return inputs;
         }
-        return inputValuesMatrix;
     };
 
     // Deklaration von Konstanten mit Klassennamen
@@ -91,7 +118,8 @@ function CustomElementsCustomizedInput() {
     return (
         <form id="formManuelInput">
             <div className={gridCell}>
-                <button onClick={handleButtonPoints} className={btnChangeCluster} type={"button"} disabled={disableButton}>{buttonInnerText}</button>
+                <button onClick={handleButtonPoints} className={btnChangeCluster} type={"button"}
+                        disabled={disableButton}>{buttonInnerText}</button>
             </div>
             {renderInputs()}
             <div className={gridCell}>
