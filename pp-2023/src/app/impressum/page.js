0dc068e82977fb1fd34057c0f518faa02e3f11b4
Darian Gerent <darian.gerent19@gmail.com>
2023-09-23 07:56:05 +0200

    feat: added impressum page

---
+++ pp-2023/src/app/impressum/page.js
@@ -0,0 +1,10 @@
+import '../globals.css';
+
+export default function Impressum() {
+    return (
+        <div id="impressum-container" className="container mh-100">
+            <h1>Impressum</h1>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </div>
+    )
+}
