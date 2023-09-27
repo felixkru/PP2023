c60a319cf446b0433329b20b3c82f3ea7be12d8c
l.knippenberg <lukas.knippenberg@noz-digital.de>
2023-09-27 15:10:04 +0200

    fix: Implementierung von KMEANS bei einer Aktion auf den Berechnen- Button.

--- pp-2023/src/app/kmeans/calculateButton.js
+++ pp-2023/src/app/kmeans/calculateButton.js
@@ -1,12 +1,36 @@
 'use client'
 import { UseInputKPoints } from './input-k-points';
+import { kMeansAlgorithm } from '../utils/kmeans';
 
 export function HandleCalculateButtonClick() {
     const { numberOfClusters } = UseInputKPoints();
 
     const handleClick = () => {
-        console.log(numberOfClusters);
+        const kPoints = validateKPoints(numberOfClusters);
+        const result = kMeansAlgorithm(dataSet, kPoints);
+        console.log(result);
+        return result;
     }
+
+    const validateKPoints = (numberOfClusters) => {
+        if (typeof numberOfClusters !== "number") {
+            return parseInt(numberOfClusters);
+        } else {
+            return numberOfClusters;
+        }
+    };
+
+    const dataSet = [
+        [2, 3, 4],
+        [2, 4, 3],
+        [3, 3, 3],
+        [4, 5, 6],
+        [5, 4, 5],
+        [6, 5, 7],
+        [9, 7, 8],
+        [10, 8, 9],
+        [11, 8, 8],
+    ];
 
     return handleClick;
 }
