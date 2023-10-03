4e0996b77fa7a3911e47894b681096cd8d8c8641
rainyy <felix_kruse@hotmail.com>
2023-10-03 21:27:11 +0200

    fix: Chart ist jetzt in der Lage den Canvas jedes mal zu befüllen wenn der Berechnenbutton verwendet wird. Außerdem einige Funktionen erzeugt/verschmälert

--- pp-2023/src/app/kmeans/scatter-chart.js
+++ pp-2023/src/app/kmeans/scatter-chart.js
@@ -7,25 +7,33 @@ function ScatterChart(numberOfClusters,calledByButton) {
 
     let myChart;
 
-    // Hier wird beim Initialen Laden der Seite das Chart erzeugt und auch wieder mit der destroy funktion zerstört.
-    // useEffect wird hier verwendet, damit sichergestellt wird, dass das Chart erst erstellt wird nachdem das DOM komplett erzeugt wurde.
-    if (calledByButton !== 1){
-    useEffect(() => {
+
+
+    const updateChart = () =>{
         const dataArray = generateDatasets(parseInt(numberOfClusters));
         let ctx = document.getElementById('myChart').getContext('2d');
-        
+        let chartStatus = Chart.getChart('myChart'); // <canvas> id
+        if (chartStatus != undefined) {
+          chartStatus.destroy();
+        }
         myChart = new Chart(ctx, dataArray);
-        myChart.destroy();
-    }, []);
+    }
+
+    // Hier wird beim Initialen Laden der Seite das Chart erzeugt und auch wieder mit der destroy funktion zerstört.
+    // useEffect wird hier verwendet, damit sichergestellt wird, dass das Chart erst erstellt wird nachdem das DOM komplett erzeugt wurde.
+    const initialChart = () =>{
+        useEffect(() => {
+            updateChart();
+        }, []);}
+
+    
+    if(calledByButton !== 1){
+        initialChart();
     }
     //Wenn die Funktion über den Berechnen Button aufgerufen wurde, dann ist die Variable calledByButton = 1 und daher wird der Canvas ohne useEffect ausgeführt. 
     //Das ist daher notwendig, da Man innerhalb des Hooks vom calculateButton keinen weiteren hook (useEffect) aufrufen kann. 
     else{
-        const dataArray = generateDatasets(parseInt(numberOfClusters));
-        let ctx = document.getElementById('myChart').getContext('2d');
-        
-        myChart = new Chart(ctx, dataArray);
-        
+       updateChart(); 
     }
 
     // returnt direkt html mit unserem chart im Canvas inklusive der Überschrift.
@@ -35,7 +43,7 @@ function ScatterChart(numberOfClusters,calledByButton) {
             <h1 className="mx-auto mt-10 text-xl font-semibold capitalize ">Scatter Chart</h1>
             <div className="h-screen flex mx-auto my-auto">
                 <div className='border border-gray-400 pt-0 rounded-xl  w-full h-fit my-auto  shadow-xl'>
-                    <canvas id='myChart'></canvas>
+                    <canvas id='myChart'/>
                 </div>
             </div>
         </>
