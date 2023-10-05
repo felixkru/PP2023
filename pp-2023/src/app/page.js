d37e56b878f944af8fd7d478378a3240560a3445
noku <noah.kuse@hs-osnabrueck.de>
2023-10-05 13:22:18 +0200

    added excel file reader

--- pp-2023/src/app/page.js
+++ pp-2023/src/app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import {useRef, useState} from 'react'
+import {useState} from 'react'
 import {SwitchVariables} from './kmeans/switch-variables';
 import {InputKPoints} from './kmeans/input-k-points';
 import {CalculateButton} from './kmeans/calculateButton';
