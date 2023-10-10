af89aa059c375c831500f123dfd64cec6d5b4cc3
l.knippenberg <lukas.knippenberg@noz-digital.de>
2023-10-10 21:02:12 +0200

    feature: Der Check, ob genug Datensätze für die Kmeans-Berechnung vorhanden sind, ist in allen Bereichen implemntiert.

--- pp-2023/src/app/kmeans/calculateButton.js
+++ pp-2023/src/app/kmeans/calculateButton.js
@@ -4,7 +4,8 @@ import {kMeansAlgorithm} from '../utils/kmeans';
 import {HandleDynamicGeneratedInputFields} from './create-save-manuel-input';
 import {apiPostRequest, handleApiCommunication, runWithTimeout, validateLengthOfData} from './requestAPI';
 import ScatterChart from './scatter-chart';
-import {returnExcel, calculateExcel} from "../utils/excelfilereader";
+import {returnExcel, calculateExcel} from '../utils/excelfilereader';
+import {APIError} from '../utils/userErrors';
 
 export function HandleCalculateButtonClick() {
 
@@ -59,6 +60,12 @@ export function HandleCalculateButtonClick() {
                 */
             } else if (localCalculation) {
                 /*
+                    Hier werden, die eingegeben Daten auf eine ausreichende Anzahl an Cluster validiert.
+                     */
+                if (validateLengthOfData(dataArrayForWorking, kPoints) === false) {
+                    return;
+                }
+                /*
                 Lokale Berechnung von KMeans mit der Visualisierung in Scatter-Chart.
                  */
                 try {
@@ -70,7 +77,7 @@ export function HandleCalculateButtonClick() {
                     console.log(result);
                     // TODO response verarbeiten
                 } catch (err) {
-                    throw new err;
+                    throw new Error(err);
                 }
             }
             /*
@@ -78,6 +85,15 @@ export function HandleCalculateButtonClick() {
             */
         } else if (inputDataSrc === "manuel") {
             if (localCalculation) {
+                /*
+                    Hier werden, die eingegeben Daten auf eine ausreichende Anzahl an Cluster validiert.
+                */
+                console.log(521)
+                const validateInputData = validateLengthOfData(inputDataArray, kPoints);
+                if (validateInputData === false) {
+                    return;
+                }
+
                 const result = await kMeansAlgorithm(inputDataArray, kPoints);
                 console.log(result)
                 ScatterChart(kPoints, chartDeletion, result);
@@ -118,7 +134,7 @@ export function HandleCalculateButtonClick() {
         } else if (inputDataArray.length !== 0) {
             return "manuel";
         } else {
-            alert(noDataMessage);
+            APIError(noDataMessage);
             return false;
         }
     };
--- pp-2023/src/app/utils/userErrors.js
+++ pp-2023/src/app/utils/userErrors.js
@@ -2,5 +2,5 @@
 Die Funktion nimmt einen Error auf
  */
 export const APIError = (error) => {
-    alert(error.detail);
+    alert(error);
 };
