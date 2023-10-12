b7a5d8a4c95e06a7d7de434d60514980c1f1aced
DarianG19 <darian.gerent19@gmail.com>
2023-10-12 14:33:07 +0200

    refactor: added comments

--- pp-2023/__tests__/SwitchVariables.test.js
+++ pp-2023/__tests__/SwitchVariables.test.js
@@ -8,7 +8,7 @@ describe('SwitchVariables Component', () => {
         <HandeleSwitchVariablesProvider>
             <SwitchVariables />
         </HandeleSwitchVariablesProvider>
-        )
+        );
 
         // Container und Label überprüfen
         const switchVariablesContainer = screen.getByText("Anzahl der Variablen");
@@ -19,54 +19,61 @@ describe('SwitchVariables Component', () => {
         const threeVariablesRadio = screen.getByLabelText("Drei Variablen");
         expect(twoVariablesRadio).toBeInTheDocument();
         expect(threeVariablesRadio).toBeInTheDocument();
-    })
+    });
 
     it('should react to click on radio buttons', () => {
         render(
-          <HandeleSwitchVariablesProvider>
+            <HandeleSwitchVariablesProvider>
             <SwitchVariables />
-          </HandeleSwitchVariablesProvider>
+            </HandeleSwitchVariablesProvider>
         );
-    
+            
+        // Überprüfen, ob Radio Buttons entsprechend checked und unchecked sind
         const twoVariablesRadio = screen.getByLabelText('Zwei Variablen');
         const threeVariablesRadio = screen.getByLabelText('Drei Variablen');
         expect(twoVariablesRadio).toBeChecked();
         expect(threeVariablesRadio).not.toBeChecked();
-    
+
+        // Klick von einem Benutzer simulieren
         fireEvent.click(threeVariablesRadio);
-    
+
+        // Neuen ausgewählten Radio Button überprüfen
         expect(twoVariablesRadio).not.toBeChecked();
         expect(threeVariablesRadio).toBeChecked();
-      });
+    });
 });
 
 describe('HandleSwitchVariables Context', () => {
     it('should provide context values', () => {
-      const { result } = renderHook(() => HandleSwitchVariables(), { wrapper: HandeleSwitchVariablesProvider });
-  
-      expect(result.current.numberOfVariables).toEqual(2);
-      expect(result.current.activePoint1).toEqual(true);
-      expect(result.current.activePoint2).toEqual(false);
+        // RenderHook-Funktion wird benötigt, um auf den Kontext der HandleSwitchVariables-Komponente zuzugreifen
+        const { result } = renderHook(() => HandleSwitchVariables(), { wrapper: HandeleSwitchVariablesProvider });
+    
+        // Überprüfen, ob die State-Variablen verfügbar sind und ihrem initialen Wert zugewiesen sind
+        expect(result.current.numberOfVariables).toEqual(2);
+        expect(result.current.activePoint1).toEqual(true);
+        expect(result.current.activePoint2).toEqual(false);
     });
   
     it('should change context values when a radio button is selected', () => {
-      const { result } = renderHook(() => HandleSwitchVariables(), { wrapper: HandeleSwitchVariablesProvider });
-  
-      /* onOptionChange triggern und überprüfen, ob die State Variablen aktualisiert entsprechend (onOptionChange setzt den Wert von numberOfVariables neu */
-      act(() => {
-        result.current.onOptionChange('3');
-      });
-  
-      expect(result.current.numberOfVariables).toEqual(3);
-      expect(result.current.activePoint1).toEqual(false);
-      expect(result.current.activePoint2).toEqual(true);
+        const { result } = renderHook(() => HandleSwitchVariables(), { wrapper: HandeleSwitchVariablesProvider });
+    
+        // onOptionChange triggern und überprüfen, ob die State Variablen aktualisiert entsprechend (onOptionChange setzt den Wert von numberOfVariables neu
+        act(() => {
+            result.current.onOptionChange('3');
+        });
+    
+        // Überprüfen, ob State-Variablen korrekt geändert werden
+        expect(result.current.numberOfVariables).toEqual(3);
+        expect(result.current.activePoint1).toEqual(false);
+        expect(result.current.activePoint2).toEqual(true);
 
-      act(() => {
-        result.current.onOptionChange('2');
-      });
-  
-      expect(result.current.numberOfVariables).toEqual(2);
-      expect(result.current.activePoint1).toEqual(true);
-      expect(result.current.activePoint2).toEqual(false);
+        // In die andere Richtung testen :)
+        act(() => {
+            result.current.onOptionChange('2');
+        });
+    
+        expect(result.current.numberOfVariables).toEqual(2);
+        expect(result.current.activePoint1).toEqual(true);
+        expect(result.current.activePoint2).toEqual(false);
     });
 });
