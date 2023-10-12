7e096416c2947c03f6706d0a9ac4808fcb0b0072
l.knippenberg <lukas.knippenberg@noz-digital.de>
2023-10-12 23:17:15 +0200

    feature: Implementierung Button-Elbow-Kriteria

---
+++ pp-2023/src/app/kmeans/elbow-kriteria.js
@@ -0,0 +1,20 @@
+export const CalculateElbowKriteria = () => {
+
+}
+
+export const CreateElbowCriteriaElements = () => {
+
+    return (
+        <div className="mt-5 mb-5 flex">
+            <div className="grid-cell-elbow">
+                <input/>
+            </div>
+            <div className="grid-cell-elbow">
+                <button className="btn btn-primary btn-lg btn-elbow" type="submit">Elbow-Kriteria</button>
+            </div>
+            <div className="grid-cell-elbow">
+
+            </div>
+        </div>
+    );
+}
