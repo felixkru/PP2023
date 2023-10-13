afd41db261e6c0f110a042cc775a615dd9101955
noku <noah.kuse@hs-osnabrueck.de>
2023-10-13 14:13:39 +0200

    fixed reading csv-files

--- pp-2023/src/app/utils/excelfilereader.js
+++ pp-2023/src/app/utils/excelfilereader.js
@@ -1,5 +1,15 @@
 import * as XLSX from "xlsx";
 
+function isCSVFile(file) {
+    const fileName = file.name;
+    return fileName.toLowerCase().endsWith('.csv');
+}
+
+function isXLSXFile(file) {
+    const fileName = file.name;
+    return fileName.toLowerCase().endsWith('.xlsx');
+}
+
 export const returnExcel = () => {
     const fileInput = document.getElementById('excelFileInput');
     const file = fileInput.files[0];
@@ -9,7 +19,7 @@ export const returnExcel = () => {
 export const calculateExcel = async () => {
     const file = returnExcel();
     console.log(file)
-    if (file) {
+    if (isXLSXFile(file)){
         const reader = new FileReader();
 
         const data = await new Promise((resolve) => {
@@ -28,10 +38,44 @@ export const calculateExcel = async () => {
 
             reader.readAsArrayBuffer(file);
         });
-        return data;
+        return (data);
+    }
+    else if (isCSVFile(file)){
+        const reader = new FileReader();
 
-    } else {
-        alert('Bitte wähle eine Excel-Datei aus.');
+        const data = await new Promise((resolve) => {
+            reader.onload = () => {
+                const text = reader.result;
+                const rows = text.split('\n');
+                const csvData = [];
+
+                rows.forEach((row) => {
+
+                    if (row.includes(";")) {
+                        const columns = row.replace('\r', '').split(';');
+                        csvData.push(columns);
+                    }
+                    else {
+                        const columns = row.replace('\r', '').split(',');
+                        csvData.push(columns);
+                    }
+                });
+
+                console.log(csvData);
+                resolve(csvData);
+            };
+
+            reader.onerror = () => {
+                console.error('Fehler beim Lesen der Datei');
+            };
+
+            reader.readAsText(file);
+        });
+
+        return (data);
+    }
+    else {
+        alert('Bitte wähle eine .xlsx oder .csv Datei aus.');
         return null; // Fehlerfall: null zurückgeben
     }
 }
