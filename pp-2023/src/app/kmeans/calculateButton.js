33858f57fa6e31f2f5c8c74fa9db6cf45fde9bd1
DarianG19 <darian.gerent19@gmail.com>
2023-10-14 14:06:13 +0200

    feature: implemented stop loading

--- pp-2023/src/app/kmeans/calculateButton.js
+++ pp-2023/src/app/kmeans/calculateButton.js
@@ -1,197 +1,212 @@
-'use client'
-import {UseInputKPoints} from './input-k-points';
-import {kMeansAlgorithm} from '../utils/kmeans';
-import {HandleDynamicGeneratedInputFields} from './create-save-manuel-input';
-import {apiPostRequest, handleApiCommunication, runWithTimeout, validateLengthOfData} from './requestAPI';
-import ScatterChart from './scatter-chart';
-import {returnExcel, calculateExcel} from '../utils/excelfilereader';
-import {APIError} from '../utils/userErrors';
-import {ExportExcelFile} from "../kmeans/exportButton";
-import { useState } from 'react';
-import { useLoadingStatus } from '../common/LoadingScreen';
-
+"use client";
+import { UseInputKPoints } from "./input-k-points";
+import { kMeansAlgorithm } from "../utils/kmeans";
+import { HandleDynamicGeneratedInputFields } from "./create-save-manuel-input";
+import {
+  apiPostRequest,
+  handleApiCommunication,
+  runWithTimeout,
+  validateLengthOfData,
+} from "./requestAPI";
+import ScatterChart from "./scatter-chart";
+import { returnExcel, calculateExcel } from "../utils/excelfilereader";
+import { APIError } from "../utils/userErrors";
+import { ExportExcelFile } from "../kmeans/exportButton";
+import { useState } from "react";
+import { useLoadingStatus } from "../common/LoadingScreen";
 
 export function HandleCalculateButtonClick(localRemoteButton) {
+  const { startLoading, stopLoading } = useLoadingStatus();
+  const { numberOfClusters } = UseInputKPoints();
+  const { inputDataArray } = HandleDynamicGeneratedInputFields();
+  const [resultExport, setResultExport] = useState([]);
 
-    const { startLoading, stopLoading } = useLoadingStatus();
-    const {numberOfClusters} = UseInputKPoints();
-    const {inputDataArray} = HandleDynamicGeneratedInputFields();
-    const [resultExport, setResultExport] = useState([]);
+  const noDataMessage =
+    "Bitte geben Sie entweder manuell Ihre Datenpunkte ein" +
+    " oder eine XLSX- CSV-Datei!";
 
-    const noDataMessage = "Bitte geben Sie entweder manuell Ihre Datenpunkte ein" +
-        " oder eine XLSX- CSV-Datei!"
+  let chartDeletion = 0;
 
-    let chartDeletion = 0;
-
-    /*
+  /*
     Die Funktion handleClick steuert als Controller die Anwendungslogik, welche Daten verwendet werden und wo diese verarbeitet werden.
      */
-    const handleClick = async () => {
+  const handleClick = async () => {
+    startLoading();
 
-        startLoading();
-        
-        /*
+    /*
         Initialisierung von Variablen für den Programmverlauf.
          */
-        const kPoints = validateKPoints(numberOfClusters);
-        const inputDataSrc = checkInputSource();
-        const localCalculation = !localRemoteButton;
-        const dataArrayForWorking = inputDataArray;
-        chartDeletion = 1; //gibt an, dass das alte Chart von der ScatterChart funktion gelöscht werden muss
+    const kPoints = validateKPoints(numberOfClusters);
+    const inputDataSrc = checkInputSource();
+    const localCalculation = !localRemoteButton;
+    const dataArrayForWorking = inputDataArray;
+    chartDeletion = 1; //gibt an, dass das alte Chart von der ScatterChart funktion gelöscht werden muss
 
-        if (inputDataSrc === "file") {
-            /*
+    if (inputDataSrc === "file") {
+      /*
             Verarbeitung eines Files und Export an die KMeans-Api, anschließend wird das Ergebnis visualisiert.
              */
-            if (!localCalculation) {
-                try {
-                    /*
+      if (!localCalculation) {
+        try {
+          /*
                     Übersenden der eingegebenen Datei an das Backend.
                      */
-                    const resultPost = await apiPostRequest(kPoints, false);
-                    /*
+          const resultPost = await apiPostRequest(kPoints, false);
+          /*
                     Hier wird der Status der Task abgefragt. Aktuell wird ein Intervall von 3000 ms berücksichtigt.
                     Der Parameter maxVersuche, gibt dabei an, wie oft ein Request wiederholt werden soll, bis dieser abbricht.
                      */
-                    if (resultPost.TaskID) {
-                        const kMeansResult = await handleApiCommunication(resultPost);
-                        //TODO Result richtig verarbeiten
-                        const localOrRemote = "remote"; // die Variable wird benötigt damit ScatterChart später weiß in welchem Format die Daten ankommen (local berechnet oder von der API)
-                        ScatterChart(kPoints, chartDeletion, kMeansResult, localOrRemote);
-                    }
-                    /*
+          if (resultPost.TaskID) {
+            const kMeansResult = await handleApiCommunication(resultPost);
+            //TODO Result richtig verarbeiten
+            const localOrRemote = "remote"; // die Variable wird benötigt damit ScatterChart später weiß in welchem Format die Daten ankommen (local berechnet oder von der API)
+            ScatterChart(kPoints, chartDeletion, kMeansResult, localOrRemote);
+          }
+          /*
                     In dem catch-Block werden allgemeine Fehler des Requests behandelt.
                     */
-                } catch (error) {
-                    stopLoading();
-                    throw new Error(error);
-                } finally {
-                    stopLoading();
-                }
-                /*
+        } catch (error) {
+          stopLoading();
+          throw new Error(error);
+        } finally {
+          stopLoading();
+        }
+        /*
                 Auslesen eines Files und anschließende Verarbeitung im Client.
                 */
-            } else if (localCalculation) {
-                /*
+      } else if (localCalculation) {
+        /*
                 Lokale Berechnung von KMeans mit der Visualisierung in Scatter-Chart.
                  */
-                try {
-                    const inputData = await calculateExcel();
-                    /*
+        try {
+          const inputData = await calculateExcel();
+          /*
                     Hier werden, die eingegeben Daten auf eine ausreichende Anzahl an Cluster validiert.
                     */
-                    if (validateLengthOfData(inputData, kPoints) === false) {
-                        return;
-                    }
-                    // TODO Generierung Ladebildschirm
-                    const timeout = 30000;
-                    const result = await runWithTimeout(kMeansAlgorithm(inputData, kPoints), timeout);
-                    const localOrRemote = "local"
-                    ScatterChart(kPoints, chartDeletion, result, localOrRemote);
-                    setResultExport(result);
-                    // TODO response verarbeiten
-                } catch (err) {
-                    stopLoading();
-                    throw new Error(err);
-                } finally {
-                    stopLoading();
-            }
-            /*
+          if (validateLengthOfData(inputData, kPoints) === false) {
+            return;
+          }
+          const timeout = 30000;
+          const result = await runWithTimeout(
+            kMeansAlgorithm(inputData, kPoints),
+            timeout
+          );
+          const localOrRemote = "local";
+          ScatterChart(kPoints, chartDeletion, result, localOrRemote);
+          setResultExport(result);
+          // TODO response verarbeiten
+        } catch (err) {
+          stopLoading();
+          throw new Error(err);
+        } finally {
+          stopLoading();
+        }
+        /*
             Verarbeitung von manuell eingegeben Daten lokal.
             */
-        } else if (inputDataSrc === "manuel") {
-            if (localCalculation) {
-                /*
+      } else if (inputDataSrc === "manuel") {
+        if (localCalculation) {
+          /*
                     Hier werden, die eingegeben Daten auf eine ausreichende Anzahl an Cluster validiert.
                 */
-                const validateInputData = validateLengthOfData(inputDataArray, kPoints);
-                if (validateInputData === false) {
-                    return;
-                }
+          const validateInputData = validateLengthOfData(
+            inputDataArray,
+            kPoints
+          );
+          if (validateInputData === false) {
+            return;
+          }
 
-                const result = await kMeansAlgorithm(inputDataArray, kPoints);
-                setResultExport(result);
-                const localOrRemote = "local";
-                ScatterChart(kPoints, chartDeletion, result, localOrRemote);
-                /*
+          const result = await kMeansAlgorithm(inputDataArray, kPoints);
+          setResultExport(result);
+          const localOrRemote = "local";
+          ScatterChart(kPoints, chartDeletion, result, localOrRemote);
+          /*
                Verarbeitung von manuell eingegeben Daten mithilfe der API.
                 */
-            } else if (!localCalculation) {
-                try {
-                    /*
+        } else if (!localCalculation) {
+          try {
+            /*
                     Übersenden der eingegebenen Datei an das Backend.
                      */
-                    const resultPost = await apiPostRequest(kPoints, dataArrayForWorking);
-                    /*
+            const resultPost = await apiPostRequest(
+              kPoints,
+              dataArrayForWorking
+            );
+            /*
                     Hier wird der Status der Task abgefragt. Aktuell wird ein Intervall von 3000 ms berücksichtigt.
                     Der Parameter maxVersuche, gibt dabei an, wie oft ein Request wiederholt werden soll, bis dieser abbricht.
                      */
-                    if (resultPost.TaskID) {
-                        const kMeansResult = await handleApiCommunication(resultPost);
-                        setResultExport(kMeansResult);
-                        const localOrRemote = "remote";
-                        ScatterChart(kPoints, chartDeletion, kMeansResult, localOrRemote);
-                        // TODO response verarbeiten
-                    }
-                    /*
+            if (resultPost.TaskID) {
+              const kMeansResult = await handleApiCommunication(resultPost);
+              setResultExport(kMeansResult);
+              const localOrRemote = "remote";
+              ScatterChart(kPoints, chartDeletion, kMeansResult, localOrRemote);
+              // TODO response verarbeiten
+            }
+            /*
                     In dem catch-Block werden allgemeine Fehler des Requests behandelt.
                     */
-                } catch (error) {
-                    stopLoading();
-                    throw new error;
-                } finally {
-                    stopLoading();
-                }
-            }
+          } catch (error) {
+            stopLoading();
+            throw new error();
+          } finally {
+            stopLoading();
+          }
         }
-        }
-    };
+      }
+    }
+    stopLoading();
+  };
 
-    /*
+  /*
     Prüft, ob die Bearbeitung von manuellen Daten erfolgt oder eines Files.
      */
-    const checkInputSource = () => {
-        if (returnExcel() !== undefined) {
-            return "file";
-        } else if (inputDataArray.length !== 0) {
-            return "manuel";
-        } else {
-            APIError(noDataMessage);
-            return false;
-        }
-    };
+  const checkInputSource = () => {
+    if (returnExcel() !== undefined) {
+      return "file";
+    } else if (inputDataArray.length !== 0) {
+      return "manuel";
+    } else {
+      APIError(noDataMessage);
+      return false;
+    }
+  };
 
-    /*
+  /*
     validateKPoints validiert die K-Points, dass diese immer als Number übergeben werden.
      */
-    const validateKPoints = (numberOfClusters) => {
-        if (typeof numberOfClusters !== "number") {
-            return parseInt(numberOfClusters);
-        } else {
-            return numberOfClusters;
-        }
-    };
+  const validateKPoints = (numberOfClusters) => {
+    if (typeof numberOfClusters !== "number") {
+      return parseInt(numberOfClusters);
+    } else {
+      return numberOfClusters;
+    }
+  };
 
-    return {
-        handleClick,
-        validateKPoints,
-        resultExport
-    };
+  return {
+    handleClick,
+    validateKPoints,
+    resultExport,
+  };
 }
 
-export function CalculateButton({localRemoteButton, setLocalRemoteButton}) {
-    const {handleClick, resultExport} = HandleCalculateButtonClick(localRemoteButton, setLocalRemoteButton);
+export function CalculateButton({ localRemoteButton, setLocalRemoteButton }) {
+  const { handleClick, resultExport } = HandleCalculateButtonClick(
+    localRemoteButton,
+    setLocalRemoteButton
+  );
 
-    return (
-        <div>
-            <button
-                type="button"
-                className='compute-btn button'
-                onClick={handleClick}
-            >
-                Berechnen
-            </button>
-            <ExportExcelFile result={resultExport}/>
-        </div>
-    );
+  return (
+    <div>
+      <button
+        type="button"
+        className="compute-btn button"
+        onClick={handleClick}
+      >
+        Berechnen
+      </button>
+      <ExportExcelFile result={resultExport} />
+    </div>
+  );
 }
--- pp-2023/src/app/utils/kmeans.js
+++ pp-2023/src/app/utils/kmeans.js
@@ -1,21 +1,27 @@
-import kMeans from 'node-kmeans';
-import {APIError} from '../utils/userErrors';
+import kMeans from "node-kmeans";
+import { APIError } from "../utils/userErrors";
+import { useLoadingStatus } from "../common/LoadingScreen";
 
 // Funktion nimmt ein DataSet entgegen, die KPoints und gibt ein Array of Arrays zurück.
 export function kMeansAlgorithm(dataset, kPoints) {
-    return kMeans.clusterize(dataset, {k: kPoints}, function (err, res) {
-        try {
-            if (res === undefined) {
-                const error = "Bitte überprüfen Sie Ihre Eingegeben Daten!" +
-                    " Eine Berechnung mit Ihrem aktuellen Datensatz ist nicht möglich.";
-                APIError(error);
-                return false;
-            } else {
-                return res;
-            }
-        } catch (err) {
-            throw new Error(err);
-        }
-    });
+  //   const { stopLoading } = useLoadingStatus();
+  return kMeans.clusterize(dataset, { k: kPoints }, function (err, res) {
+    try {
+      if (res === undefined) {
+        const error =
+          "Bitte überprüfen Sie Ihre Eingegeben Daten!" +
+          " Eine Berechnung mit Ihrem aktuellen Datensatz ist nicht möglich.";
+        APIError(error);
+        return false;
+      } else {
+        return res;
+      }
+    } catch (err) {
+      //   stopLoading();
+      throw new Error(err);
+      // } finally {
+      //   stopLoading();
+      // }
+    }
+  });
 }
-
